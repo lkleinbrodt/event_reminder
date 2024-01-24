0c7a79f22d0226fb747d6b341b6a8a47b826e6d3
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2024-01-24 08:52:46 -0800

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    ENV = os.environ.get('ENV', 'dev').lower()
+    assert ENV in ['dev', 'prod', 'test']
+    if SECRET_KEY is None:
+        raise ValueError("No SECRET_KEY set for Flask application")
+    
+    if ENV == 'prod':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(ROOT_DIR, 'app.db')
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(ROOT_DIR, 'dev_app.db')
+
+    TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+    TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+    TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+    TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
+    
+    MAIL_SERVER = None
---
+++ todo.md
@@ -0,0 +1,13 @@
+To Do List:
+
+<>1. Ability to remove dates from a users list
+<>2. Better phone number entry (log in)
+<>3. User Settings (1,3,7 day reminders)
+<>better styling
+4. Text message delivery
+<>. Code Cleanup
+6. Prepare for deployment
+7. Backing up database to Prod
+8. Admin panel to force a database save
+
+
