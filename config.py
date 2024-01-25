1d45f48f56b59be0335600ff022b23d29e3ebbd1
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2024-01-25 09:53:04 -0800

    gotta fix postgres url

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(ROOT_DIR, 'app.db')
     else:
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
 
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
     TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
@@ -24,6 +25,6 @@ class Config:
     
     MAIL_SERVER = None
     
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     S3_BUCKET = os.environ.get('S3_BUCKET')
