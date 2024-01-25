0b8a1c6d3f751a6a24867128ecd2ad3d8015975b
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2024-01-24 21:05:18 -0800

    logging for backup

--- app/backup_database.py
+++ app/backup_database.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 from app.s3 import S3
 from pytz import timezone
+from app import app
 
 def save_db_to_s3():
+    app.logger.exception('TRYING TO BACKUP DB TO S3')
     s3 = S3()
     today = datetime.now(timezone('America/Los_Angeles')).date()
-    s3.upload_file('app.db', f'app_{today}.db')
-    # try:
-    # except:
-    #     app.logger.exception('Failed to upload app.db to S3')
+    try:
+        s3.upload_file('app.db', f'app_{today}.db')
+    except:
+        app.logger.exception('Failed to upload app.db to S3')
     return True
 
 if __name__ == '__main__':
-    save_db_to_s3()
+    with app.app_context():
+        save_db_to_s3()
